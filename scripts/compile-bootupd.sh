808d78e10d1a2a56359873c3c30814c99d825e5a
krolmiki2011 <mikolajziolkowski504@gmail.com>
2026-05-09 14:55:21 +0200

    Update

--- scripts/compile-bootupd.sh
+++ scripts/compile-bootupd.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 git clone https://github.com/coreos/bootupd.git .
 wget https://raw.githubusercontent.com/PhantomOS-Linux/bootupd-archlinux-patch/refs/heads/main/0001-archlinux.patch
 
-patch -Np1 -i ../0001-archlinux.patch
+patch -Np1 -i 0001-archlinux.patch
 export RUSTUP_TOOLCHAIN=stable
 export CARGO_TARGET_DIR=target
 make
