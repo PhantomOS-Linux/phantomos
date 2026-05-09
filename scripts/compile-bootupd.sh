ac2fbb7a20eaf236ab830e07f7e3e5e1964f876a
krolmiki2011 <mikolajziolkowski504@gmail.com>
2026-05-09 19:23:01 +0200

    facepalm

--- scripts/compile-bootupd.sh
+++ scripts/compile-bootupd.sh
@@ -12,7 +12,6 @@ make
 
 RUST_BACKTRACE=1 cargo test
 make DESTDIR=/output LIBEXECDIR=/usr/lib install
-rm /output/usr/lib/bootupd
 mv /output/usr/lib/bootupd /output/usr/bin/bootupctl
 make DESTDIR=/output LIBEXECDIR=/usr/lib install-grub-static
 make DESTDIR=/output LIBEXECDIR=/usr/lib install-systemd-unit
