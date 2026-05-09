a69efc8d75ea8c81371a242405717723efe2b283
krolmiki2011 <mikolajziolkowski504@gmail.com>
2026-05-09 18:19:03 +0200

    Fix

--- scripts/compile-bootc.sh
+++ scripts/compile-bootc.sh
@@ -5,3 +5,6 @@ set -xeuo pipefail
 git clone "https://github.com/bootc-dev/bootc.git" .
  
 make bin install-all DESTDIR=/output
+
+mkdir -p /output/usr/lib/libostree
+cp -rv /output/usr/libexec/libostree/* /output/usr/lib/libostree
